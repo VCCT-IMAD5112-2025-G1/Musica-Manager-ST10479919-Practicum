e5eb20c2f615479dda88e8a16027e7e43f5b02d9
Jaco <ST10479919@vcconnect.edu.za>
2025-06-19 10:44:09 +0200

    Finished DataInput Created layout for DetailedView

--- app/src/main/java/za/co/varsitycollege/st10479919/musicamanager/DataInput.kt
+++ app/src/main/java/za/co/varsitycollege/st10479919/musicamanager/DataInput.kt
@@ -1,10 +1,14 @@
+// Petrus Jakobus Venter ST10479919
 package za.co.varsitycollege.st10479919.musicamanager
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RatingBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,17 +25,25 @@ class DataInput : AppCompatActivity() {
             insets
         }
 
+        var titleArray = arrayOf<String>("", "", "", "");
+        var artistArray = arrayOf<String>("","","","");
+        var songRatingArray = IntArray(4);
+        var songCommentArray = arrayOf<String>("","","","");
+
+        // Displays help for user
+        val userHelpText = findViewById<TextView>(R.id.userHelpText);
+
         // Display and input for Song Title
         val songTitleText = findViewById<TextView>(R.id.songTitleText);
         val songTitleInput = findViewById<EditText>(R.id.titleInput);
         // Display and input for Artist
-        val songArtist = findViewById<TextView>(R.id.artistNameText);
+        val songArtistText = findViewById<TextView>(R.id.artistNameText);
         val songArtistInput = findViewById<EditText>(R.id.artistInput);
         // Display and input for Rating
-        val songRating = findViewById<TextView>(R.id.ratingText);
+        val songRatingText = findViewById<TextView>(R.id.ratingText);
         val songRatingInput = findViewById<RatingBar>(R.id.ratingInput);
         // Display and input for Comment
-        val songComment = findViewById<TextView>(R.id.userCommentText);
+        val songCommentText = findViewById<TextView>(R.id.userCommentText);
         val songCommentInput = findViewById<EditText>(R.id.commentInput);
 
         // Button to confirm input data
@@ -41,6 +53,84 @@ class DataInput : AppCompatActivity() {
         // Button to exit app
         val exitButton = findViewById<Button>(R.id.exitButton);
 
+        // Incrementor
+        var i = 0;
+
+        toDetailedViewButton.isEnabled = false;
+
+        // Button to store data
+        confirmButton.setOnClickListener {
+            // Input validation
+            if (i < titleArray.size && isValidString(songTitleInput.text.toString()) == true && isValidString(songArtistInput.text.toString()) == true && isValidString(songCommentInput.text.toString()) == true){
+                userHelpText.text = "Song data ${i + 1} Added."
+
+                // Add elements to array from input
+                titleArray[i] = songTitleInput.text.toString()
+                Log.v("Title added:", "${titleArray[i]} at ${i}")
+                artistArray[i] = songArtistInput.text.toString()
+                Log.v("Artist added:", "${artistArray[i]} at ${i}")
+                songRatingArray[i] = songRatingInput.rating.toInt()
+                Log.v("Rating added:", "${songRatingArray[i]} at ${i}")
+                songCommentArray[i] = songCommentInput.text.toString()
+                Log.v("Comment added:", "${songCommentArray[i]} at ${i}")
+                // Increment to next subscript
+                i++
+                // Clear all inputs
+                songTitleInput.text.clear()
+                songArtistInput.text.clear()
+                songRatingInput.rating = 0.0F
+                songCommentInput.text.clear()
+                // User completed populating arrays
+                if (i == 4) {
+                    userHelpText.text = "Please press the Review button to check your data."
+                    // Disable button for input
+                    confirmButton.isEnabled = false;
+                    // Enable button to go to next screen
+                    toDetailedViewButton.isEnabled = true;
+                }
+            }
+            else if (i < titleArray.size) {
+
+                Toast.makeText(this,"Please enter valid items!", Toast.LENGTH_SHORT).show();
+                Log.v("User Error:", "Invalid input");
+                Log.v("Title:", songTitleInput.text.toString());
+                Log.v("Artist:", songArtistInput.text.toString());
+                Log.v("Rating:", songRatingInput.rating.toInt().toString());
+                Log.v("Comment:",songCommentInput.text.toString());
+            }
+            // In case user in error state
+            else {
+                userHelpText.text = "Please press the Review button to check your data."
+                // Disable button for input
+                confirmButton.isEnabled = false;
+                // Enable button to go to next screen
+                toDetailedViewButton.isEnabled = true;
+            }
+
+        }
+
+        // Moves to the DetailedView screen to display data that was inputted
+        toDetailedViewButton.setOnClickListener {
+            val toDetailedView = Intent(this, DetailedView::class.java);
+            toDetailedView.putExtra("titleArray", titleArray);
+            toDetailedView.putExtra("artistArray", artistArray);
+            toDetailedView.putExtra("songRatingArray", songRatingArray);
+            toDetailedView.putExtra("songCommentArray", songCommentArray);
+            startActivity(toDetailedView);
+            finish();
+        }
+
+        // Exits the app
+        exitButton.setOnClickListener {
+            finish();
+        }
+
+    }
+    fun isValidString(input: String): Boolean {
+        if (input.isNotEmpty() && input != null) {
+            return true
+        }
+        else return false;
     }
 
 }
--- app/src/main/java/za/co/varsitycollege/st10479919/musicamanager/DetailedView.kt
+++ app/src/main/java/za/co/varsitycollege/st10479919/musicamanager/DetailedView.kt
@@ -1,12 +1,23 @@
+// Petrus Jakobus Venter ST10479919
 package za.co.varsitycollege.st10479919.musicamanager
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class DetailedView : AppCompatActivity() {
+
+    // Late declare for use inside entire class
+    lateinit var  songTextView: ArrayList<TextView>
+    lateinit var  artistTextView: ArrayList<TextView>
+    lateinit var  ratingTextView: ArrayList<TextView>
+    lateinit var  commentTextView: ArrayList<TextView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,56 @@ class DetailedView : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Populate TextView arrays for looping
+        findViewById<TextView>(R.id.songNameText0) ?. let { songTextView.add(it) };
+        findViewById<TextView>(R.id.songNameText1) ?. let { songTextView.add(it) };
+        findViewById<TextView>(R.id.songNameText2) ?. let { songTextView.add(it) };
+        findViewById<TextView>(R.id.songNameText3) ?. let { songTextView.add(it) };
+
+        findViewById<TextView>(R.id.artistNameText0) ?. let { artistTextView.add(it) };
+        findViewById<TextView>(R.id.artistNameText1) ?. let { artistTextView.add(it) };
+        findViewById<TextView>(R.id.artistNameText2) ?. let { artistTextView.add(it) };
+        findViewById<TextView>(R.id.artistNameText3) ?. let { artistTextView.add(it) };
+
+        findViewById<TextView>(R.id.ratingText0) ?. let { ratingTextView.add(it) };
+        findViewById<TextView>(R.id.ratingText1) ?. let { ratingTextView.add(it) };
+        findViewById<TextView>(R.id.ratingText2) ?. let { ratingTextView.add(it) };
+        findViewById<TextView>(R.id.ratingText3) ?. let { ratingTextView.add(it) };
+
+        findViewById<TextView>(R.id.commentText0) ?. let { commentTextView.add(it) };
+        findViewById<TextView>(R.id.commentText1) ?. let { commentTextView.add(it) };
+        findViewById<TextView>(R.id.commentText2) ?. let { commentTextView.add(it) };
+        findViewById<TextView>(R.id.commentText3) ?. let { commentTextView.add(it) };
+
+        val populateButton = findViewById<Button>(R.id.populateButton);
+        val calculateAvgButton = findViewById<Button>(R.id.calculateAvgButton);
+        val toHomeButton = findViewById<Button>(R.id.returnToMainScreenButton);
+
+
+
+        // Get title from parsed data
+        val titleArray = intent.getStringArrayExtra("titleArray")!!;
+        for (i in 0 until titleArray.count()) {
+            Log.v("Title array", "${titleArray[i]}")
+        }
+        // Get artists from parsed data
+        val artistArray = intent.getStringArrayExtra("artistArray")!!;
+        for (i in 0 until artistArray.count()){
+            Log.v("Artist array", "${artistArray[i]}")
+        }
+        // Get rating from parsed data
+        val songRatingArray = intent.getIntArrayExtra("songRatingArray")!!;
+        for (i in 0 until songRatingArray.count()) {
+            Log.v("Song Rating", "${songRatingArray[i]}");
+        }
+        // Get comment from parsed data
+        val songCommentArray = intent.getStringArrayExtra("songCommentArray")!!;
+        for (i in 0 until songCommentArray.count()) {
+            Log.v("Song comment", "${songCommentArray[i]}");
+        }
+
+
+
     }
 }
